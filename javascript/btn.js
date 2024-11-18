e87741e262eab70e1945a2b01b5837554aba42df
kaur072005 <jasreenk033@gmail.com>
2024-11-18 11:29:31 +0530

    Delete javascript directory

--- javascript/btn.js
+++
@@ -1,5 +0,0 @@
-let button = document.querySelector("#button")
-button.addEventListener("click",function(){
-    console.log("button was clicked");
-alert("button clicked!!");}
-);
--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-let username=25;
-let gamename= prompt("guess a number");
-
-while(gamename!=username)
-{
-    gamename=prompt("incorrect number !! Guess the number again");
-}
-
-console.log("congratulatoions !!  you won");
